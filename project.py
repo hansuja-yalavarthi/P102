f8de10c861dc4751ecfd0885c26a88adb1bc89af
hansuja-yalavarthi <78600451+hansuja-yalavarthi@users.noreply.github.com>
2021-12-30 09:31:51 -0600

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 #This is "auto-renaming"
 import os
-originalfilename=input("Enter File Path with File Name")
-newfilename=input("Enter the File Path with New File Name")
+originalfilename=input("Enter File Path with File Name: ")
+newfilename=input("Enter the File Path with New File Name: ")
 os.rename(originalfilename,newfilename)
 print("file renamed succesfully")
